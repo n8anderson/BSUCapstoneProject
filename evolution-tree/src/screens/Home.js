27e4cda7245db43385a118d18ea889adcb090a35
Nate Anderson <n8anderson@student.bridgew.edu>
2023-05-10 10:18:15 -0400

    Overflow fix

--- evolution-tree/src/screens/Home.js
+++ evolution-tree/src/screens/Home.js
@@ -6,20 +6,18 @@ import './Home.scss'
 function Home() {
 
   return (
-    <div className="App">
-        <motion.div
-          className="container text-center"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 1 }}
-        >
-        <div className="App-body">
-          <NavigationBar />
-          <EvolutionaryTree />
-        </div>
-      </motion.div>
-    </div>
+    <motion.div
+      className="home"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <div className="home-body">
+        <NavigationBar />
+        <EvolutionaryTree />
+      </div>
+    </motion.div>
   );
 }
 
